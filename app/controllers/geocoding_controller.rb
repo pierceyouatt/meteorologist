fefbcca77611b660196c180254f02074e16faead
Raghu Betina <raghu@firstdraft.com>
2018-01-25 14:55:05 -0600

    Copy outline

---
+++ app/controllers/geocoding_controller.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+
+class GeocodingController < ApplicationController
+  def street_to_coords_form
+    # Nothing to do here.
+    render("street_to_coords_form.html.erb")
+  end
+
+  def street_to_coords
+    @street_address = params[:user_street_address]
+    url_safe_street_address = URI.encode(@street_address)
+
+    # ==========================================================================
+    # Your code goes below.
+    # The street address the user input is in the string @street_address.
+    # A URL-safe version of the street address, with spaces and other illegal
+    #   characters removed, is in the string url_safe_street_address.
+    # ==========================================================================
+
+    @street_address = params[:user_street_address]
+
+    # ==========================================================================
+    # Your code goes below.
+    #
+    # The street address that the user typed is in the variable @street_address.
+    # ==========================================================================
+
+
+
+    @latitude = "Replace this string with your answer."
+
+    @longitude = "Replace this string with your answer."
+
+    render("geocoding/street_to_coords.html.erb")
+  end
+end
