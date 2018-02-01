f27b9d14307dd35270b1c45bc61ba668c5390341
Murugan <murugan@firstdraft.com>
2018-02-01 04:51:01 +0000

    Assignment complete, full credit

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -25,11 +25,13 @@ class GeocodingController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
 
+    parsed_data = JSON.parse(open(url).read)
 
-    @latitude = "Replace this string with your answer."
+    @latitude = parsed_data.dig("results", 0, "geometry", "location", "lat")
 
-    @longitude = "Replace this string with your answer."
+    @longitude = parsed_data.dig("results", 0, "geometry", "location", "lng")
 
     render("geocoding/street_to_coords.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,23 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
+    parsed_data = JSON.parse(open(url).read)
+    @latitude = parsed_data.dig("results", 0, "geometry", "location", "lat")
+    @longitude = parsed_data.dig("results", 0, "geometry", "location", "lng")
+    
+    second_url = "https://api.darksky.net/forecast/eef2b1f8ad4a93bcf2088939dafc46f4/#{@latitude},#{@longitude}"
+    met_parsed_data = JSON.parse(open(second_url).read)
+    
+    @current_temperature = met_parsed_data.dig("currently", "temperature")
 
+    @current_summary = met_parsed_data.dig("currently", "summary")
 
-    @current_temperature = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = met_parsed_data.dig("minutely", "summary")
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_several_hours = met_parsed_data.dig("hourly", "summary")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = met_parsed_data.dig("daily", "summary")
 
     render("meteorologist/street_to_weather.html.erb")
   end
